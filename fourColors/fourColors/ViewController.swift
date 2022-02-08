1a57e68bae55feb13709fb6f90bac440ea07975d
Ömer <omeroguzcelikel@gmail.com>
2022-02-09 01:21:05 +0300

    thirdRectangle

---
+++ fourColors/fourColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fourColors
+//
+//  Created by Ömer Oğuz Çelikel on 9.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
