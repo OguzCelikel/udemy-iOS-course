2a6665035582b7366ac78f6477738419f88c5360
Ömer <omeroguzcelikel@gmail.com>
2022-02-23 15:08:56 +0300

    tryAgain

---
+++ guessTheNumbersGame/guessTheNumbersGame/SoncuEkraniVC.swift
@@ -0,0 +1,29 @@
+//
+//  SoncuEkraniVC.swift
+//  guessTheNumbersGame
+//
+//  Created by Ömer Oğuz Çelikel on 22.02.2022.
+//
+
+import UIKit
+
+class SoncuEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
